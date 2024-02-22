62d8837768620ea32d919382ecc5799a9406d9d4
bemadeveloper <akbermet1990@icloud.com>
2024-02-22 21:16:26 +0600

    Creating firstButton and adding asset for it

--- NavigationAnimation/Sources/ViewController.swift
+++ NavigationAnimation/Sources/ViewController.swift
@@ -3,6 +3,31 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - UI
+    private lazy var firstButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Facebook", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        button.setImage(UIImage(named: "image"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.imageEdgeInsets = UIEdgeInsets(top: 12, left: -40, bottom: 12, right: 10)
+        
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        button.titleEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 30)
+        button.layer.cornerRadius = 20
+        button.configuration?.imagePadding = 100
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 10
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+      
+        return button
+    }()
     
     // MARK: - Lyfecycle
     
@@ -19,10 +44,21 @@ class ViewController: UIViewController {
         view.backgroundColor = .systemYellow
     }
     
-    private func setupHierarchy() {}
+    private func setupHierarchy() {
+        view.addSubview(firstButton)
+    }
     
-    private func setupLayout() {}
+    private func setupLayout() {
+        
+    }
     
     // MARK: - Actions
+    @objc
+    private func buttonTapped() {
+        if let url = URL(string: "https://mobdevcourse.com/pl/teach/control/lesson/view?id=302908760&editMode=0") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        print("Button tapped")
+    }
 }
 

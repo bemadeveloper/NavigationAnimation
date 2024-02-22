a6001cb2dcd47107d3ed84601ec7a08be0faab05
bemadeveloper <akbermet1990@icloud.com>
2024-02-22 20:54:49 +0600

    ViewController Settings

--- NavigationAnimation/Sources/ViewController.swift
+++ NavigationAnimation/Sources/ViewController.swift
@@ -1,12 +1,28 @@
-//
-//  ViewController.swift
-//  NavigationAnimation
-//
-//  Created by Bema on 22/2/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    // MARK: - Lyfecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setups
+    
+    private func setupView() {
+        view.backgroundColor = .systemYellow
+    }
+    
+    private func setupHierarchy() {}
+    
+    private func setupLayout() {}
+    
+    // MARK: - Actions
 }
 
